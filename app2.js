ae70ccd44ea5b2af67609d06bf4be347f4275b34
AY <amin.yassin@live.com>
2016-06-25 14:39:31 +0200

    Click button only once, add songs, edit some code and other minor stuff

--- app2.js
+++ app2.js
@@ -6,17 +6,36 @@ function page(){
 }
 
 var songs = [
-    'assets/sample.mp3',
-    'assets/sample2.mp3',
-    'assets/sample4.wav',
-    'assets/sample3.mp3'
+
+    'assets/beyonce/crzy_n_luv.mp3',
+    'assets/beyonce/gul.mp3',
+    'assets/beyonce/halo.mp3',
+    'assets/beyonce/independent_wmn.mp3',
+    'assets/beyonce/los_m_brth.mp3',
+    'assets/beyonce/nnn.mp3',
+    'assets/Janet/Alright.mp3',
+    'assets/Janet/Black Cat.mp3',
+    'assets/Janet/Control.mp3',
+    'assets/Janet/Escapade.mp3',
+    'assets/Janet/Love Will Never Do.mp3',
+    'assets/Janet/Miss You Much.mp3',
+    'assets/Janet/Rhythm Nation.mp3',
+    'assets/Janet/Runaway.mp3',
+    'assets/Janet/Thats The Way Love Goes.mp3',
+    'assets/Janet/The Pleasure Principal.mp3',
+    'assets/Janet/What Have You Done For Me Lately.mp3',
+    'assets/Janet/When I Think Of You.mp3',
+    'assets/beyonce/luvntp.mp3'
 ];
 
+console.log(songs.length);
+
 function playSong(){
-    var randomSong = (Math.floor(Math.random() * 4));
+    var randomSong = (Math.floor(Math.random() * songs.length));
     sample = new Audio (songs[randomSong]);
     sample.play();
-    
+    document.getElementById('audioP').disabled = true;
+
     var barProg = document.getElementById("bar");
     barProg.style.backgroundColor = 'crimson'; 
     var width = 1;
@@ -34,8 +53,6 @@ function playSong(){
 }
 
 function stopSong(){
-    if (!sample.paused){
-        sample.pause();
-        console.log('stopping a playing song!');
-    }
+    document.getElementById('audioP').disabled = false;
+    sample.pause();
 }
